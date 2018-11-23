f36d7a370fddabe87153aec91dee388398ed0a22
Bdanhub <45082436+Bdanhub@users.noreply.github.com>
2018-11-23 12:37:18 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,70 @@
+coconst Discord = require('discord.js');
+const client = new Discord.Client();
+const convert = require("hh-mm-ss")
+const dateFormat = require('dateformat');
+const fs = require('fs');
+const pretty = require('pretty-ms');
+const rn = require('random-number');
+const userData = JSON.parse(fs.readFileSync('./userData.json', 'utf8'));
+const moment = require('moment');
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const prefix = "#";
+let done = {};
+
+          const devs = ['411137717884289024' , '345152850751389697' , '' , ''];
+          const adminprefix = "#";
+          client.on('message', message => {
+              var argresult = message.content.split(`).slice(1).join(' ');
+                if (!devs.includes(message.author.id)) return;
+
+            if (message.content.startsWith(adminprefix + 'st')) {
+              client.user.setGame(argresult, "https://www.twitch.tv/ حب بلا حدود");
+                message.channel.sendMessage(:white_check_mark:   ${argresult}`)
+            }
+            });
+
+
+
+
+
+
+
+
+const developers = ["411137717884289024","345152850751389697",""]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Done.   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Done.   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Done.   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Done.**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
