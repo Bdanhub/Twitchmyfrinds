42733b7876a82dfe1b5441a217dd5a1ddbd5dd29
Bdanhub <45082436+Bdanhub@users.noreply.github.com>
2018-11-23 12:51:36 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,36 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const convert = require("hh-mm-ss")
-const dateFormat = require('dateformat');
-const fs = require('fs');
-const pretty = require('pretty-ms');
-const rn = require('random-number');
-const userData = JSON.parse(fs.readFileSync('./userData.json', 'utf8'));
-const moment = require('moment');
-var Canvas = require('canvas')
-var jimp = require('jimp')
+const tpoints = {};
+const vpoints = {};
+const jimp = require("jimp");
 const prefix = "#";
+const client = new Discord.Client();
 let done = {};
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setActivity("",{type: 'watching'})
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
 
           const devs = ['411137717884289024' , '345152850751389697' , '' , ''];
           const adminprefix = "#";
